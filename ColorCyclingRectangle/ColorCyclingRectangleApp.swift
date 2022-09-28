0f8879c98cd04f08b3c20592b9e03a044fa0cf39
Carlossiii <cv5673@gmail.com>
2022-09-28 18:25:15 -0300

    Initial Commit

---
+++ ColorCyclingRectangle/ColorCyclingRectangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCyclingRectangleApp.swift
+//  ColorCyclingRectangle
+//
+//  Created by Carlos Vinicius on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorCyclingRectangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
